400bd91d768a851e70b14031ab18388f123f54b5
cesarcuadro <cesarcuadro777@gmail.com>
2024-09-14 17:26:04 -0700

    added methods and its corresponding psuedocode

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,2 +1,20 @@
 public class LinkedList {
+    public Node headNode;
+
+    public void addNode(int x, int y) {
+        // create new node with x and y params
+        // set nextNode on new node object to existing headNode object
+        // assign new node object to headNode variable
+    }
+
+    public void removeNode() {
+        // needs to change current head node that is in nextNode variable
+        // needs to return that previous head node to the method caller
+        // essentially, taking head node out of linked list and returning it
+            // and setting next node in line as the new head node
+    }
+
+    public Node getHeadNode() {
+        return headNode;
+    }
 }
