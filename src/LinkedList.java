957ceabd5ff3a49b4ba2129b5354693fe6ceb905
cesarcuadro <cesarcuadro777@gmail.com>
2024-09-14 17:45:30 -0700

    added Node constructor. coded addNode method

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -3,13 +3,18 @@ public class LinkedList {
 
     public void addNode(int x, int y) {
         // create new node with x and y params
+        Node newNode = new Node(x, y);
         // set nextNode on new node object to existing headNode object
+        newNode.nextNode = headNode;
         // assign new node object to headNode variable
+        headNode = newNode;
     }
 
     public void removeNode() {
         // needs to change current head node that is in nextNode variable
+        headNode = headNode.nextNode;
         // needs to return that previous head node to the method caller
+
         // essentially, taking head node out of linked list and returning it
             // and setting next node in line as the new head node
     }
--- src/Node.java
+++ src/Node.java
@@ -2,4 +2,9 @@ public class Node {
     public int xPosition;
     public int yPosition;
     public Node nextNode;
+
+    public Node (int xPosition, int yPosition) {
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+    }
 }
